0b07c26b0abfa0ecadda05ec1834620267eeda4d
Maksym "Max" Pikhteryev <maksym.p@gmail.com>
2015-09-20 08:38:06 -0400

    final

--- fw/worker.c
+++ fw/worker.c
@@ -5,9 +5,10 @@
  */
 #include <pebble_worker.h>
 #define ACCEL_DATA 0
+// #define TIMER_REFRESH 100
+
   
-  AppTimer *refreshTimer;
-#define TIMER_REFRESH 100
+AppTimer *refreshTimer;
 
 DataLoggingSessionRef accel_data_logger;
 
@@ -15,22 +16,23 @@ DataLoggingSessionRef accel_data_logger;
   
 // }
 
-void timer_callback(){
-  data_logging_finish(accel_data_logger);
-  accel_data_logger = data_logging_create(1, DATA_LOGGING_BYTE_ARRAY, sizeof(AccelData), true); 
+// void timer_callback(){
+//   data_logging_finish(accel_data_logger);
+//   accel_data_logger = data_logging_create(1, DATA_LOGGING_BYTE_ARRAY, sizeof(AccelData), true); 
   
-  refreshTimer = app_timer_register(TIMER_REFRESH, timer_callback, NULL);
-}
+//   refreshTimer = app_timer_register(TIMER_REFRESH, timer_callback, NULL);
+// }
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
   
-  data_logging_log(accel_data_logger, data, num_samples);
+//   data_logging_log(accel_data_logger, data, num_samples);
   // Construct a data packet
   AppWorkerMessage xyz_data = {
     .data0 = data[0].x,
     .data1 = data[0].y, 
-    .data2 = data[0].z
+    .data2 = data[0].z,
   };
+  
   // Send the data to the foreground app
   app_worker_send_message(ACCEL_DATA, &xyz_data);
 }
@@ -41,14 +43,14 @@ static void worker_init() {
     accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
   
     // create data logger
-    accel_data_logger = data_logging_create(1, DATA_LOGGING_BYTE_ARRAY, sizeof(AccelData), true); 
+//     accel_data_logger = data_logging_create(1, DATA_LOGGING_BYTE_ARRAY, sizeof(AccelData), true); 
   
 //   app_worker_message_subscribe(worker_message_handler);
   
-    refreshTimer = app_timer_register(TIMER_REFRESH, timer_callback, NULL);
+//     refreshTimer = app_timer_register(TIMER_REFRESH, timer_callback, NULL);
 }
 static void worker_deinit() {
-    data_logging_finish(accel_data_logger);
+//     data_logging_finish(accel_data_logger);
     accel_data_service_unsubscribe();
 }
 int main(void) {
